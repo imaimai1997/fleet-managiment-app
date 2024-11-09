75b17944de2bf7ea4f4f1bf26e1e3377d8144454
imaimai <onakasuitanaapple@gmail.com>
2024-11-09 11:41:12 +0900

    primary colorの設定

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,11 +10,26 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+          primary: {
+                '50': '#edfff5',
+                '100': '#d6ffea',
+                '200': '#afffd6',
+                '300': '#71ffb7',
+                '400': '#2dfb91',
+                '500': '#02e571',
+                '600': '#00bf5a',
+                '700': '#00984b',
+                '800': '#06753d',
+                '900': '#085f34',
+                '950': '#00361b',
+            },
+          
       },
     },
   },
+  
   plugins: [],
+  
+    
 };
 export default config;
