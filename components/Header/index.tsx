589090bb922f6d7879e77d72ea79753472180531
imaimai1997 <onakasuitanaapple@gmail.com>
2024-11-09 12:18:08 +0900

    primary colorの設定 (#34)

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -12,20 +12,20 @@ const Header = () => {
 
   return (
     <>
-      <div className="bg-emerald-600 flex justify-between items-center px-20 py-2">
+      <div className="bg-primary-700 flex justify-between items-center h-14 px-16">
         <h2 className="text-white text-4xl">Logo</h2>
-        <div className="relative text-white">
+        <div className="relative h-10 text-white ">
           <button onClick={handleUserMenu}>
             <IoPersonCircle size={40} />
           </button>
           {isUserMenu && (
             <div className="bg-gray-300 absolute right-1/2 translate-x-2/4 w-40 flex flex-col items-center px-4 py-4 rounded-3xl text-black font-bold">
               <p>USER NAME</p>
-              <button className="bg-emerald-600 flex items-center px-4 py-2 my-2 rounded-3xl hover:bg-emerald-400">
+              <button className="bg-primary-700 flex items-center px-4 py-2 my-2 rounded-3xl hover:bg-primary-500">
                 SETTING
                 <IoSettingsSharp className="ml-2" />
               </button>
-              <button className="bg-emerald-600 flex items-center px-4 py-2 rounded-3xl hover:bg-emerald-400">
+              <button className="bg-primary-700 flex items-center px-4 py-2 rounded-3xl hover:bg-primary-500">
                 LOGOUT
                 <TbDoorExit className="ml-2" />
               </button>
