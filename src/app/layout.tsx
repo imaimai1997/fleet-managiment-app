3f958ec92929cdd7e99087f3b157516a58714ea3
imaimai1997 <onakasuitanaapple@gmail.com>
2024-11-17 11:35:46 +0900

    SearchBarの作成 (#38)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import Header from "../../components/Header";
-import Sidebar from "../../components/Sidebar";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -32,9 +31,7 @@ export default function RootLayout({
       >
         <Header />
 
-
         <div>{children}</div>
-
       </body>
     </html>
   );
