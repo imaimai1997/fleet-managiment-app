4f08d34c21003592ca529e3dc386ccda8301d550
imaimai <onakasuitanaapple@gmail.com>
2024-11-26 23:24:08 +0900

    型データ作成

---
+++ type/UserData.ts
@@ -0,0 +1,11 @@
+export type UserData ={
+  id: string;
+    role:{
+      id:number;
+      name:string;
+    } ,
+    user_name:string;
+    email:string;
+    created_at: Date;
+    updated_at: Date;
+    }
