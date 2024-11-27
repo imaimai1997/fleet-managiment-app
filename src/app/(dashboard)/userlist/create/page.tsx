0b5a09809b2aa150d3b3df4187ef5ddc9983e96a
imaimai <onakasuitanaapple@gmail.com>
2024-11-27 20:04:41 +0900

    ユーザー作成画面

---
+++ src/app/(dashboard)/userlist/create/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import UserDetail from "../../../../../components/UserDetail";
+
+const page = () => {
+  return (
+    <>
+      {" "}
+      <h2 className="m-16 text-xl">ユーザー追加</h2>
+      <UserDetail />
+    </>
+  );
+};
+
+export default page;
