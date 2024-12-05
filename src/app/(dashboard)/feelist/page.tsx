090c2819b82379ab460b7c4b31b6da61efb71c26
imaimai1997 <onakasuitanaapple@gmail.com>
2024-12-05 20:29:59 +0900

    [FE]データ取込画面の作成 (#52)
    * データインポート画面の作成
    
    * 取込ボタンの設置

--- src/app/(dashboard)/feelist/page.tsx
+++ src/app/(dashboard)/feelist/page.tsx
@@ -1,5 +1,7 @@
+import Link from "next/link";
 import FeeSearch from "../../../../components/FeeSearch";
 import FeeTable from "../../../../components/FeeTable";
+import PrimaryButton from "../../../../components/PrimaryButton";
 
 const FeeListPage = async () => {
   const res = await fetch("http://localhost:3000/testdata/cardata.json");
@@ -12,6 +14,13 @@ const FeeListPage = async () => {
     <>
       <FeeSearch carData={data} />
       <FeeTable feeData={feeData} />
+      <div className="w-5/6 fixed bottom-0 py-2 bg-white shadow-inner">
+        <div className="flex justify-end max-w-5xl mx-auto">
+          <Link href="/feelist/import">
+            <PrimaryButton name="データ取込" />
+          </Link>
+        </div>
+      </div>
     </>
   );
 };
