620301e273d4610cac2498d917668cc739175cbc
imaimai <onakasuitanaapple@gmail.com>
2024-12-26 08:22:35 +0900

    ユーザー情報の削除

--- src/app/api/user/[id]/route.ts
+++ src/app/api/user/[id]/route.ts
@@ -32,3 +32,31 @@ export const GET = async (req: Request) => {
     await prisma.$disconnect();
   }
 };
+
+//ユーザー情報削除
+
+export const DELETE = async (req: Request) => {
+  const id = parseInt(req.url.split("/user/")[1]);
+  try {
+    await main();
+    const user = await prisma.user.delete({
+      where: { id: id },
+    });
+    return NextResponse.json(
+      { message: "Success", user },
+      {
+        status: 200,
+      },
+    );
+  } catch (err) {
+    console.log(err);
+    return (
+      NextResponse.json({ message: "Error", err }),
+      {
+        status: 500,
+      }
+    );
+  } finally {
+    await prisma.$disconnect();
+  }
+};
