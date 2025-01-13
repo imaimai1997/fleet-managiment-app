f85c3c4665fde70a75ce023f4639c60e40977fa9
imaimai <onakasuitanaapple@gmail.com>
2025-01-13 09:48:33 +0900

    認証情報の反映

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,27 +1,53 @@
 "use client";
+import { useAuthContext } from "@/context/authContext";
+import { auth } from "@/utils/firebase";
+import { signOut } from "firebase/auth";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 import React, { useState } from "react";
+import toast, { Toaster } from "react-hot-toast";
 import { IoPersonCircle } from "react-icons/io5";
 import { IoSettingsSharp } from "react-icons/io5";
 import { TbDoorExit } from "react-icons/tb";
 
 const Header = () => {
+  const router = useRouter();
+
   const [isUserMenu, setIsUserMenu] = useState(false);
   const handleUserMenu = () => {
     setIsUserMenu(!isUserMenu);
   };
+  const authContext = useAuthContext();
+  if (!authContext) {
+    return <div>Loading...</div>;
+  }
+  const { currentUser } = authContext;
+
+  const handleLogout = async () => {
+    try {
+      await signOut(auth);
+      toast.success("ログアウトしました", { id: "1" });
+      router.push("/signin");
+      router.refresh();
+    } catch (err) {
+      console.error("Error:", err);
+
+      toast.error("ログアウトがうまくいきませんでした。", { id: "1" });
+    }
+  };
 
   return (
     <>
+      <Toaster />
       <div className="bg-primary-700 flex justify-between items-center h-14 px-16">
-        <h2 className="text-white text-4xl">Logo</h2>
+        <h2 className="text-white text-2xl italic">FLEET-MANAGIMENT</h2>
         <div className="relative h-10 text-white ">
           <button onClick={handleUserMenu}>
             <IoPersonCircle size={40} />
           </button>
           {isUserMenu && (
-            <div className="bg-gray-300 absolute right-1/2 translate-x-2/4 w-40 flex flex-col items-center px-4 py-4 rounded-3xl text-black font-bold">
-              <p>USER NAME</p>
+            <div className="bg-gray-300 absolute right-1/2 translate-x-2/4 w-40 flex flex-col items-center px-4 py-4 rounded-3xl text-black font-bold z-10">
+              <p>{currentUser?.name}</p>
               <Link href="/setting">
                 <button
                   onClick={handleUserMenu}
@@ -31,7 +57,10 @@ const Header = () => {
                   <IoSettingsSharp className="ml-2" />
                 </button>
               </Link>
-              <button className="bg-primary-700 flex items-center px-4 py-2 rounded-3xl hover:bg-primary-500">
+              <button
+                onClick={handleLogout}
+                className="bg-primary-700 flex items-center px-4 py-2 rounded-3xl hover:bg-primary-500"
+              >
                 LOGOUT
                 <TbDoorExit className="ml-2" />
               </button>
