3574d79f35a7a4142584d933d13e3aa1741e77e8
imaimai1997 <onakasuitanaapple@gmail.com>
2025-02-07 22:32:04 +0900

    [BE]検索機能の追加 (#112)
    * 検索機能の追加
    
    * 型注釈の変更
    
    * serchParamsの非同期処理の修正

--- components/SearchBar/index.tsx
+++ components/SearchBar/index.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+"use client";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import React, { useState } from "react";
 import { IoIosSearch } from "react-icons/io";
 
 const SearchBar = () => {
+  const [term, setTerm] = useState<string>();
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  const handleSearch = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    const params = new URLSearchParams(searchParams);
+    params.set("page", "1");
+    if (term) {
+      params.set("query", term);
+    } else {
+      params.delete("query");
+    }
+    replace(`${pathname}?${params.toString()}`);
+  };
   return (
     <>
       <form>
@@ -9,10 +27,12 @@ const SearchBar = () => {
           <input
             type="search"
             placeholder="Search"
+            onChange={(e) => setTerm(e.target.value)}
+            defaultValue={searchParams.get("query")?.toString()}
             className="bg-gray-200 w-80 p-4 pr-14 rounded-3xl focus:ring-4 focus:outline-none focus:ring-primary-700 "
           />
           <button
-            type="submit"
+            onClick={handleSearch}
             className="absolute top-1/2 right-0 h-full px-4 rounded-r-3xl transform -translate-y-1/2 group-focus-within:bg-primary-700"
             aria-label="Search"
           >
