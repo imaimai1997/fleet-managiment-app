dcf263c4e82e9879de112031f565c26da9590f4e
imaimai <onakasuitanaapple@gmail.com>
2025-02-07 22:10:20 +0900

    検索機能の追加

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,25 +1,41 @@
 import React from "react";
-// import SearchBar from "../../../components/SearchBar";
+import SearchBar from "../../../components/SearchBar";
 import CarList from "../../../components/CarList";
 import PrimaryButton from "../../../components/PrimaryButton";
 import Link from "next/link";
+import { CarData } from "../../../type/CarData";
 
-const fetchCarList = async () => {
+type Props = {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+};
+
+const fetchFilteredCars = async (query: string) => {
   const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/car`, {
     cache: "no-store",
   });
 
   const data = await res.json();
-  return data.cars;
+  const cars = await data.cars;
+  const filteredCar = await cars.filter(
+    (car: CarData) =>
+      car.label.toLowerCase().includes(query.toLowerCase()) ||
+      car.employee.name.toLowerCase().includes(query.toLowerCase()),
+  );
+
+  return filteredCar;
 };
 
-const CarListPage = async () => {
-  const cars = await fetchCarList();
+const CarListPage = async ({ searchParams }: Props) => {
+  const query = (await searchParams?.query) || "";
+  const cars = await fetchFilteredCars(query);
 
   return (
     <>
       <div className="mx-8 mt-8 mb-16">
-        {/* <SearchBar /> 開発中*/}
+        <SearchBar />
         <CarList data={cars} />
       </div>
       <div className="w-5/6 fixed bottom-0 text-end px-16 py-2 bg-white">
