c23d4f0e558c357254763213fb79c5ffade1b10d
shtk0llq <shtk0llq@gmail.com>
2025-03-16 16:59:31 +0900

    component name change

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -4,7 +4,7 @@ import Header from "../../../components/Header";
 import Sidebar from "../../../components/Sidebar";
 import { AuthProvider } from "../../context/authContext";
 
-const RootLayout = ({
+const DashboardLayout = ({
   children,
 }: Readonly<{
   children: React.ReactNode;
@@ -24,4 +24,5 @@ const RootLayout = ({
     </>
   );
 };
-export default RootLayout;
+
+export default DashboardLayout;
