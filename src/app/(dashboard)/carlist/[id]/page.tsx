76355801ed4cfd7638597187d6d4bc2a86f0e885
shtk0llq <shtk0llq@gmail.com>
2025-03-17 00:09:04 +0900

    carlist code fix

--- src/app/(dashboard)/carlist/[id]/page.tsx
+++ src/app/(dashboard)/carlist/[id]/page.tsx
@@ -1,22 +1,21 @@
-import React from "react";
 import CarDetail from "../../../../../components/CarDetail";
 
-const fetchCarById = async (id: string) => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/car/${id}`, {
+const fetchCarData = async (id: string) => {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/car/${id}`, {
     cache: "no-store",
   });
 
-  const data = await res.json();
-  return data.car;
-};
+  const { car } = await response.json();
+  return car;
+}
 
 const CarDetailPage = async ({
-  params,
+  params
 }: {
-  params: Promise<{ id: string }>;
+  params: Promise<{ id: string }>
 }) => {
-  const id = (await params).id;
-  const carData = await fetchCarById(id);
+  const { id } = await params;
+  const carData = await fetchCarData(id);
 
   return (
     <>
