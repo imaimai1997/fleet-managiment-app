42152f3116a7b177fe7658dfe0f01cb67e7e617e
imaimai <onakasuitanaapple@gmail.com>
2025-04-01 22:31:24 +0900

    燃費ページのUI修正

--- src/components/GasMileageList/_components/GasMileageTable/index.tsx
+++ src/components/GasMileageList/_components/GasMileageTable/index.tsx
@@ -24,97 +24,96 @@ const GasMileageTable = ({ gasMileageData }: Props) => {
   );
 
   return (
-    <div className="w-11/12 mx-auto m-4 mb-24">
-      <table className="w-full text-left mt-8">
-        <thead>
-          <tr>
-            <th
-              scope="col"
-              className="sticky top-0 bg-gray-400 px-6 py-3 w-1/5"
-            >
-              年月
-            </th>
-            <th
-              scope="col"
-              className="sticky top-0 bg-gray-400 px-6 py-3 w-1/5"
-            >
-              車両番号
-            </th>
-            <th
-              scope="col"
-              className="sticky top-0 bg-gray-400 px-6 py-3 w-1/5"
-            >
-              走行距離
-            </th>
-            <th
-              scope="col"
-              className="sticky top-0 bg-gray-400 px-6 py-3 w-1/5"
-            >
-              給油量
-            </th>
-            <th
-              scope="col"
-              className="sticky top-0 bg-gray-400 px-6 py-3 w-1/5"
-            >
-              燃費
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {gasMileageData.map((data) => (
-            <tr
-              key={data.car_number}
-              className="border-b-2 hover:bg-primary-100"
-            >
-              <th scope="row" className="px-6 py-2">
-                {formatDateToYearMonth(data.year_month)}
+    <>
+      <div className="bg-white p-8 mx-8 mt-4 mb-16 rounded-md border-2 border-gray-200">
+        <table className="w-full text-left mt-2">
+          <thead>
+            <tr>
+              <th
+                scope="col"
+                className="sticky top-0 px-6 py-3 border-b border-gray-200 bg-white font-light text-gray-500"
+              >
+                年月
               </th>
-              <td className="px-6 py-2">{data.car_number}</td>
-
-              <td className="px-6 py-2">{data.mileage_total_mileage}km</td>
-              <td className="px-6 py-2">{data.refueling_total_amount}L</td>
-
-              <td className="px-6 py-2">
-                {(
-                  data.mileage_total_mileage / data.refueling_total_amount
-                ).toFixed(2)}
-                km/L
-              </td>
+              <th
+                scope="col"
+                className="sticky top-0 px-6 py-3 border-b border-gray-200 bg-white font-light text-gray-500"
+              >
+                車両番号
+              </th>
+              <th
+                scope="col"
+                className="sticky top-0 px-6 py-3 border-b border-gray-200 bg-white font-light text-gray-500"
+              >
+                走行距離
+              </th>
+              <th
+                scope="col"
+                className="sticky top-0 px-6 py-3 border-b border-gray-200 bg-white font-light text-gray-500"
+              >
+                給油量
+              </th>
+              <th
+                scope="col"
+                className="sticky top-0 px-6 py-3 border-b border-gray-200 bg-white font-light text-gray-500"
+              >
+                燃費
+              </th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {gasMileageData.map((data) => (
+              <tr
+                key={data.car_number}
+                className="border-b-2 hover:bg-primary-100"
+              >
+                <th scope="row" className="px-6 py-4">
+                  {formatDateToYearMonth(data.year_month)}
+                </th>
+                <td className="px-6 py-4">{data.car_number}</td>
 
-      <div className="w-5/6 h-12 mx-auto fixed bottom-0 bg-white shadow-inner">
-        <div className="w-11/12">
-          <table className="w-full text-left">
-            <tbody>
-              <tr>
-                <td className="px-6 py-2 w-2/5 text-right text-primary-700 font-bold">
-                  平均燃費
-                </td>
+                <td className="px-6 py-4">{data.mileage_total_mileage}km</td>
+                <td className="px-6 py-4">{data.refueling_total_amount}L</td>
 
-                <td className="px-6 py-2 w-1/5 font-bold">
-                  {totalGasMileage
-                    ? `${totalGasMileage.mileage.toLocaleString()}km`
-                    : "0km"}
-                </td>
-                <td className="px-6 py-2 w-1/5 font-bold">
-                  {totalGasMileage
-                    ? `${totalGasMileage.refuelingAmount.toLocaleString()}L`
-                    : "0L"}
-                </td>
-                <td className="px-6 py-2 w-1/5 font-bold">
-                  {totalGasMileage
-                    ? `${(totalGasMileage.mileage / totalGasMileage.refuelingAmount).toFixed(2)}km/L`
-                    : "0km/L"}
+                <td className="px-6 py-4">
+                  {(
+                    data.mileage_total_mileage / data.refueling_total_amount
+                  ).toFixed(2)}
+                  km/L
                 </td>
               </tr>
-            </tbody>
-          </table>
-        </div>
+            ))}
+          </tbody>
+        </table>
       </div>
-    </div>
+      <div className="w-[calc(100vw-240px)] h-12 fixed bottom-0 bg-white border-t-2 border-gray-200">
+        <table className="w-full text-left">
+          <tbody>
+            <tr>
+              <td className="px-6 py-2 w-2/5 text-right text-primary-700 font-bold">
+                平均燃費
+              </td>
+
+              <td className="px-6 py-2 w-1/5 font-bold">
+                {totalGasMileage
+                  ? `${totalGasMileage.mileage.toLocaleString()}km`
+                  : "0km"}
+              </td>
+              <td className="px-6 py-2 w-1/5 font-bold">
+                {totalGasMileage
+                  ? `${totalGasMileage.refuelingAmount.toLocaleString()}L`
+                  : "0L"}
+              </td>
+              <td className="px-6 py-2 w-1/5 font-bold">
+                {totalGasMileage
+                  ? `${(totalGasMileage.mileage / totalGasMileage.refuelingAmount).toFixed(2)}km/L`
+                  : "0km/L"}
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </>
   );
 };
 
