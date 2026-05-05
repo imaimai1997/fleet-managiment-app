d65acb51d7f1024610afbad6064298756efbecc6
mai <onakasuitanaapple@gmail.com>
2026-05-05 19:40:54 +0900

    環境変数の非公開設定

--- src/utils/adminFirebase.ts
+++ src/utils/adminFirebase.ts
@@ -1,16 +1,16 @@
 const serviceAccount = {
   project_id: "fleet-managiment",
-  private_key_id: process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY_ID,
-  privateKey: process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY?.replace(
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(
     /\\n/g,
     "\n",
   ),
-  client_email: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_EMAIL,
-  client_id: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_ID,
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
   auth_uri: "https://accounts.google.com/o/oauth2/auth",
   token_uri: "https://oauth2.googleapis.com/token",
   auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-  client_x509_cert_url: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_X509_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
   universe_domain: "googleapis.com",
 };
 import admin from "firebase-admin";
